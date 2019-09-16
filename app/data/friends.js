17a925255aeea39574a2697e2dd847067a94638b
Zhen Cui <zcui1989@gmail.com>
2019-09-16 13:37:05 -0400

    add users

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,26 @@
 var friendsData = [
     {
       name: 'Chan',
+      scores:[1,2,3,4,5]
     },
     {
       name: 'Zhao',
+      scores:[2,2,3,4,5]
+
     },
     {
       name: 'Lee',
+      scores:[1,2,5,4,5]
+
     },
     {
       name: 'Wang',
+      scores:[1,2,3,2,5]
+
     },
     {
       name: 'Zhang',
+      scores:[1,2,3,1,5]
     },
 ];
   
